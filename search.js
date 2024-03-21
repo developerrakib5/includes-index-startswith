21f2d26880b4ca40671fedc0db831f956a071b20
devlrakib <devrakib5@gmail.com>
2024-03-21 11:32:22 +0600

    first commit

---
+++ search.js
@@ -0,0 +1,19 @@
+
+
+// =========================> Includes Js Code <=======================
+const lyrics = 'Tumi Bondhu kala Pakhi Ami Jeno Ki bosonto kal a tumake bolte pari ni';
+const doesExist = lyrics.includes('Pakhi');  // Case Sensitive
+console.log(doesExist);
+
+// =========================> Index Of Js Code <=======================
+console.log(lyrics.indexOf('Tumi'));
+console.log(lyrics.indexOf('kal'));
+
+// =========================> Startswith Js Code <=======================
+console.log(lyrics.startsWith('Tumi'));
+
+// =========================> Endswith Js Code <=======================
+const fileName = 'mybiodata.pdf';
+const otherFile = 'mypic.png';
+console.log(fileName.endsWith('.pdf'));
+
